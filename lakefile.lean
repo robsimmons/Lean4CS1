5140948f212a10935513616960c29dda9b39b81c
Rob Simmons <rob@lean-fro.org>
2026-04-08 14:28:30 -0400

    switch order of dependences to avoid pinning issue

--- lakefile.lean
+++ lakefile.lean
@@ -3,9 +3,9 @@ open Lake DSL
 
 package «fp-course» where
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "v4.28.0"
-
 require verso from git "https://github.com/leanprover/verso.git" @ "v4.28.0"
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "v4.28.0"
 
 @[default_target]
 lean_lib «FPCourse» where
